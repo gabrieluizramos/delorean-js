3f2841614728b0041b8a00ee6691d66ed4ab23b4
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2017-11-08 14:58:40 -0200

    bugfixes at functions

--- js/timemachine.js
+++ js/timemachine.js
@@ -1,192 +1,190 @@
 var timemachine = (function timeMachine(today, locale) {
 
-      'use strict';
+  var _date = today;
+  var _locale = locale;
 
-      var _date = today;
-      var _locale = locale;
+  // Bug fixes for IE
+  var _navigatorIsIE = window.navigator.userAgent.indexOf('MSIE') > 0;
+  var _IEMonths = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+  var _IEWeekDays = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'];
 
-      // Bug fixes for IE
-      var _navigatorIsIE = window.navigator.userAgent.indexOf('MSIE') > 0;
-      var _IEMonths = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
-      var _IEWeekDays = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'];
+  function _get(whatYouWant, type) {
+    var dateObj = { timeZone: 'UTC' };
+    dateObj[whatYouWant] = type;
+    // verify if user is in IE
+    return _navigatorIsIE ? _getForIE(whatYouWant, type) : _date.toLocaleDateString(_locale, dateObj);
+  }
 
-      function _get(whatYouWant, type) {
-        var dateObj = {timeZone: 'UTC'};
-        dateObj[whatYouWant] = type;
-        // verify if user is in IE
-        return _navigatorIsIE ? _getForIE(whatYouWant, type) : _date.toLocaleDateString(_locale, dateObj);
-      }
+  function _getForIE(whatYouWant, type) {
+    var toLocaleString;
 
-      function _getMonthByNumber (number) {
-        number--;
-        return ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'][number];
-      }
+    switch (whatYouWant) {
+      // weekday
+      case 'weekday':
+        toLocaleString = _date.getDay();
 
-      function _getForIE(whatYouWant, type) {
-        var toLocaleString;
-
-        switch (whatYouWant) {
-          // weekday
-          case 'weekday':
-            toLocaleString = _date.getDay();
-
-            switch (type) {
-              case 'short':
-                toLocaleString = _IEWeekDays[toLocaleString].substring(0, 3);
-                break;
-
-              case 'long':
-                toLocaleString = _IEWeekDays[toLocaleString];
-                break;
-            }
+        switch (type) {
+          case 'short':
+            toLocaleString = _IEWeekDays[toLocaleString].substring(0, 3);
             break;
-          // end weekday
 
-          // day
-          case 'day':
-            toLocaleString = _date.getDate();
-
-            switch (type) {
-              case '2-digit':
-                toLocaleString = toLocaleString < 10 ? '0' + toLocaleString : toLocaleString;
-                break;
-            }
+          case 'long':
+            toLocaleString = _IEWeekDays[toLocaleString];
             break;
-          // end day
-
-          // month
-          case 'month':
-            toLocaleString = _date.getMonth();
-
-            switch (type) {
-              case 'short':
-                toLocaleString = _IEMonths[toLocaleString].substring(0, 3);
-                break;
-
-              case 'long':
-                toLocaleString = _IEMonths[toLocaleString];
-                break;
-
-              case 'numeric':
-                toLocaleString++;
-                break;
-
-              case '2-digit':
-                toLocaleString++;
-                toLocaleString = toLocaleString < 10 ? '0' + toLocaleString : toLocaleString;
-                break;
-            }
-            break;
-          // end month
-
-          // year
-          case 'year':
-            toLocaleString = _date.getFullYear();
-
-            switch (type) {
-              case '2-digit':
-                toLocaleString = Number(String(toLocaleString).substring(2, 4));
-                break;
-            }
-            break;
-          // end year
         }
+        break;
+      // end weekday
 
-        return toLocaleString;
-      }
+      // day
+      case 'day':
+        toLocaleString = _date.getDate();
 
-      function _setDate(date) {
-        date += 'T00:00:00';
-        _date = new Date(date);
-
-        return true;
-      }
-
-      function _setLocale(locale) {
-        _locale = locale;
-      }
-
-      function _reboot() {
-        _date = new Date();
-      }
-
-      function _getTime() {
-        return _date.getHours() + 'h' + _date.getMinutes() + 'min' + _date.getSeconds() + 'seg';
-      }
-
-      function _getISODate() {
-        return _get('year', 'numeric') + '-' + _get('month', '2-digit') + '-' + _get('day', '2-digit');
-      }
-
-      function _getLocale() {
-        return _locale;
-      }
-
-      function _getPrimitiveDate() {
-        return _date;
-      }
-
-      function _getFullDate() {
-        return _get('day', '2-digit') + '/' + _get('month', '2-digit') + '/' + _get('year', 'numeric');
-      }
-
-      function _getYear(type) {
-        return _get('year', (type || 'numeric'));
-      }
-
-      function _getMonth(type) {
-        return _get('month', (type || 'long'));
-      }
-
-      function _getDay(type) {
-        return _get('day', (type || 'numeric'));
-      }
-
-      function _getWeekDay(type) {
-        return _get('weekday', (type || 'long'));
-      }
-
-      function _getAllMonthsOfYear(type) {
-        var monthsNames = [];
-        var realDate = (_date.getMonth() + 1) + '/' + _date.getDate() + '/' + _date.getFullYear();
-
-        for (var months = 1; months <= 12; months++) {
-          _setDate(months + '/01/2017');
-          monthsNames.push(_getMonth(type));
+        switch (type) {
+          case '2-digit':
+            toLocaleString = toLocaleString < 10 ? '0' + toLocaleString : toLocaleString;
+            break;
         }
+        break;
+      // end day
 
-        _setDate(realDate);
+      // month
+      case 'month':
+        toLocaleString = _date.getMonth();
 
-        return monthsNames;
-      }
+        switch (type) {
+          case 'short':
+            toLocaleString = _IEMonths[toLocaleString].substring(0, 3);
+            break;
 
-      return !_navigatorIsIE ?
-        {
-          getTime: _getTime,
-          getPrimitiveDate: _getPrimitiveDate,
-          getDay: _getDay,
-          getWeekDay: _getWeekDay,
-          getMonth: _getMonth,
-          getAllMonthsOfYear: _getAllMonthsOfYear,
-          getYear: _getYear,
-          getFullDate: _getFullDate,
-          getISODate: _getISODate,
-          setDate: _setDate,
-          getLocale: _getLocale,
-          setLocale: _setLocale,
-          reboot: _reboot,
-          getISODate: _getISODate,
-          getMonthByNumber: _getMonthByNumber
-        } :
-        {
-          getDay: _getDay,
-          getWeekDay: _getWeekDay,
-          getMonth: _getMonth,
-          setDate: _setDate,
-          getYear: _getYear,
-          getFullDate: _getFullDate,
-          getISODate: _getISODate,
-          getMonthByNumber: _getMonthByNumber
-        };
+          case 'long':
+            toLocaleString = _IEMonths[toLocaleString];
+            break;
 
-    })(new Date(), 'pt-br');
+          case 'numeric':
+            toLocaleString++;
+            break;
+
+          case '2-digit':
+            toLocaleString++;
+            toLocaleString = toLocaleString < 10 ? '0' + toLocaleString : toLocaleString;
+            break;
+        }
+        break;
+      // end month
+
+      // year
+      case 'year':
+        toLocaleString = _date.getFullYear();
+
+        switch (type) {
+          case '2-digit':
+            toLocaleString = Number(String(toLocaleString).substring(2, 4));
+            break;
+        }
+        break;
+      // end year
+    }
+
+    return toLocaleString;
+  }
+
+  function _setDate(date) {
+    date += 'T00:00:00';
+    _date = new Date(date);
+
+    return true;
+  }
+
+  function _setLocale(locale) {
+    _locale = locale;
+  }
+
+  function _reboot() {
+    _date = new Date();
+  }
+
+  function _getTime() {
+    return _date.getHours() + 'h' + _date.getMinutes() + 'min' + _date.getSeconds() + 'seg';
+  }
+
+  function _getISODate() {
+    return _get('year', 'numeric') + '-' + _get('month', '2-digit') + '-' + _get('day', '2-digit');
+  }
+
+  function _getLocale() {
+    return _locale;
+  }
+
+  function _getPrimitiveDate() {
+    return _date;
+  }
+
+  function _getFullDate() {
+    return _get('day', '2-digit') + '/' + _get('month', '2-digit') + '/' + _get('year', 'numeric');
+  }
+
+  function _getYear(type) {
+    return _get('year', (type || 'numeric'));
+  }
+
+  function _getMonth(type) {
+    return _get('month', (type || 'long'));
+  }
+
+  function _getDay(type) {
+    return _get('day', (type || 'numeric'));
+  }
+
+  function _getWeekDay(type) {
+    return _get('weekday', (type || 'long'));
+  }
+
+  function _getAllMonthsOfYear(type) {
+    var monthsNames = [];
+    var realYear = _date.getFullYear();
+    var realMonth = (_date.getMonth() + 1);
+    realMonth = realMonth < 10 ? '0' + realMonth : realMonth;
+    var realDay = _date.getDate();
+    realDay = realDay < 10 ? '0' + realDay : realDay;
+
+    var realDate = realYear + '-' + realMonth + '-' + realDay;
+
+    for (var months = 1; months <= 12; months++) {
+      months = months < 10 ? '0' + months : months;
+      _setDate('2017-' + months + '-01');
+      monthsNames.push(_getMonth(type));
+    }
+
+    _setDate(realDate);
+
+    return monthsNames;
+  }
+
+  return !_navigatorIsIE ?
+    {
+      getTime: _getTime,
+      getPrimitiveDate: _getPrimitiveDate,
+      getDay: _getDay,
+      getWeekDay: _getWeekDay,
+      getMonth: _getMonth,
+      getAllMonthsOfYear: _getAllMonthsOfYear,
+      getYear: _getYear,
+      getFullDate: _getFullDate,
+      getISODate: _getISODate,
+      setDate: _setDate,
+      getLocale: _getLocale,
+      setLocale: _setLocale,
+      reboot: _reboot,
+      getISODate: _getISODate
+    } :
+    {
+      getDay: _getDay,
+      getWeekDay: _getWeekDay,
+      getMonth: _getMonth,
+      setDate: _setDate,
+      getYear: _getYear,
+      getFullDate: _getFullDate,
+      getISODate: _getISODate
+    };
+
+})(new Date(), 'pt-br');
